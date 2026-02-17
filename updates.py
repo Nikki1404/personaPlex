8a02384531601055beeb8efaf04695acb4001998
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-17 22:15:39 +0530

    Create updates.py

---
+++ updates.py
@@ -0,0 +1,8 @@
+2026-02-17 16:43:53,683 [INFO] [main] Starting moshi.server: moshi.server --host 0.0.0.0 --port 8000 --hf-repo nvidia/personaplex-7b-v1 --device cuda --static none
+usage: python [-h] [--host HOST] [--port PORT] [--static STATIC]
+              [--gradio-tunnel] [--gradio-tunnel-token GRADIO_TUNNEL_TOKEN]
+              [--tokenizer TOKENIZER] [--moshi-weight MOSHI_WEIGHT]
+              [--mimi-weight MIMI_WEIGHT] [--hf-repo HF_REPO]
+              [--device DEVICE] [--cpu-offload]
+              [--voice-prompt-dir VOICE_PROMPT_DIR] [--ssl SSL]
+python: error: unrecognized arguments: -m moshi.server
